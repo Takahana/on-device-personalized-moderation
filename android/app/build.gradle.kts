c67faf23bdef1bd5d9126d05e56ac0a52fd0756d
Takahana <takahana.go@gmail.com>
2026-08-29 19:50:19 +0900

    feat: hilt-lifecycle-viewmodel-compose の依存追加

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
 
   // Arch Components
   implementation(libs.androidx.lifecycle.runtime.compose)
