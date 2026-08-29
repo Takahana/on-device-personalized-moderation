2b5f4ef84478e73ace5f199f4e73e9a375ab6bae
Takahana <takahana.go@gmail.com>
2026-08-29 19:49:11 +0900

    feat: デバッグビルド以外で無効になる Logger の追加

---
+++ android/app/src/main/java/com/example/mystream/logger/Logger.kt
@@ -0,0 +1,13 @@
+package com.example.mystream.logger
+
+import android.util.Log
+import com.example.mystream.BuildConfig
+
+class Logger(
+    private val tag: String,
+) {
+    fun d(message: String) {
+        if (!BuildConfig.DEBUG) return
+        Log.d(tag, message)
+    }
+}
