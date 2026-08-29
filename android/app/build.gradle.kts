9b5b0ad56ca6370d8532aa687bc5b1e4cd80264e
Takahana <takahana.go@gmail.com>
2026-08-30 08:32:28 +0900

    fix: エミュレーターを起動しているマシンのローカルサーバーにアクセスするときのURLを例示

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
             buildConfigField(
                 "String",
                 "LIVE_CHAT_URL",
+                // エミュレーターを起動しているマシンのローカルサーバーにアクセスする場合は ws://10.0.2.2:8080
                 "\"ws://localhost:8080\""
             )
         }
